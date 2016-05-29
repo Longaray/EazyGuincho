877cc08e96debf77d7fd4a9f66e3ab1c728899cc
Longaray <rodrigo.longaray@acad.pucrs.br>
2016-05-29 17:09:41 -0300

    Ionic - EazyGuincho
     + Adding 2 templates : guincho.hmtl review.html
    	- review.html needs working stars mechanics for avaliation.
    	- guincho has ugly desing, needs idea.
     + Cadastro now opens a new window - guincho with more details of the "guincheiro".
    	- after selecting a "guincheiro" a new window open were you confim the arrival and review the
    	  service.

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -44,38 +44,41 @@ angular.module('starter', ['ionic'])
     $urlRouterProvider.otherwise('/index');
 })
 
-.controller('IndexControl', function($scope, $state) {
-        $scope.getList = function() {
-            //alert('Index Alert ng-click')
-            console.log('Index - getListMethod');
-            $state.go('lists');
-        };
-})
-
 //TODO: Corrigir o template list.html para ter o mesmo valor
 .controller('ListCtrl', function($scope,$ionicModal) {
-	$scope.toDoListItems = [
+	$scope.listItems = [
   {
-      placa: 'HTM 2175',
-      nome: 'Zé do Guincho',
-      cnh: '1111muitaroda',
-      cpf: '0888888888888-25',
-      tds: $scope.tds
-  }, {
+    plate: 'HTM 2175',
+    time: '30 minutos',
+    name: 'Zé do Guincho',
+    cnh: '1111muitaroda',
+    cpf: '0111111111111-11',
+    value: '500',
+    rank: '5'
+  },
+  {
     name: 'Tony Reboque',
     time: '40 minutos',
-    value: 'R$ 400',
+    plate: 'BMW 8888',
+    cnh: '8888888888888',
+    cpf: '0888888888888-88',
+    value: '300',
+    rank: '5'
   },
-   {
+  {
     name: 'Pedro Te Busca',
-    time: '30 minutos',
-    value: 'R$ 300',
+    time: '20 minutos',
+    plate: 'IJM 1515',
+    cnh: 'javenceu37',
+    cpf: '37373737373737-27',
+    value: '300',
+    rank: '5'
   }];
   
-  $scope.tds = [
-    { text: "Carro", checked: true },
-    { text: "Caminhão", checked: false },
-    { text: "Moto", checked: false }
+  $scope.ServiceType = [
+    { text: "Carro", checked: true, km: 10.00, hour: 10.00 },
+    { text: "Caminhão", checked: false, km: 50.00, hour: 50.00},
+    { text: "Moto", checked: false, km: 5.00, hour: 5.00 }
   ];
 
  //Pega a lista com os guinchos
@@ -92,40 +95,92 @@ angular.module('starter', ['ionic'])
       animation: 'slide-in-up'
  }).then(function(modal) { $scope.modal2 = modal;});
  
+  $ionicModal.fromTemplateUrl('/templates/guincho.html', {
+      id: '3',
+      scope: $scope,
+      animation: 'slide-in-up'
+ }).then(function(modal) { $scope.modal3 = modal;});
+ 
+  $ionicModal.fromTemplateUrl('/templates/review.html', {
+      id: '4',
+      scope: $scope,
+      animation: 'slide-in-up'
+  }).then(function(modal) { $scope.modal4 = modal;});
+ 
  //Método de abrir os modais
- $scope.openModal = function(index) {
+ $scope.openModal = function(index, data) {
+      $scope.guincho = data;
       if (index == 1) $scope.modal1.show();
-      else $scope.modal2.show();
+      if (index == 2) $scope.modal2.show();
+      if (index == 3) $scope.modal3.show();
+      if (index == 4) $scope.modal4.show();
   };
   
  //Método de fechar os modais
- $scope.closeModal = function() {
+ $scope.closeModal = function(index) {
+   if (index == 1) $scope.modal1.hide();
+   if (index == 2) $scope.modal2.hide();
+   if (index == 3) $scope.modal3.hide();
+   if (index == 4) $scope.modal4.hide();
+ };
+ 
+  $scope.closeAllModal = function() {
    $scope.modal1.hide();
    $scope.modal2.hide();
+   $scope.modal3.hide();
+   $scope.modal4.hide();
  };
  
  //Cleanup the modal when we're done with it!
  $scope.$on('$destroy', function() {
    $scope.modal1.remove();
    $scope.modal2.remove();
+   $scope.modal3.remove();
+   $scope.modal4.remove();
  });
 
+//Carro
+// -- p/km
+// -- p/h
  //TODO: Achar uma maneira menos tosca
  //TODO: checkbox not saving
  //Adiciona Guincho na Lista
  $scope.Save = function(data){
-    $scope.toDoListItems.push({
-      placa: data.placa,
-      nome: data.nome,
+    $scope.listItems.push({
+      plate: data.placa,
+      name: data.nome,
       cnh: data.cnh,
       cpf: data.cpf,
-      tds: data.tds
+      carro_km: data.carro_km,
+      carro_hora: data.carro_hora,
+      caminhao_km: data.caminhao_km,
+      caminhao_hora: data.caminhao_hora,
+      moto_km: data.moto_km,
+      moto_hora: data.moto_hora
     });
     data.placa = '';
     data.nome = '';
     data.cnh = '';
     data.cpf = '';
-    data.tds = $scope.tds;
-    $scope.closeModal();
+    data.carro_km = '';
+    data.carro_hora = '';
+    data.caminhao_km = '';
+    data.caminhao_hora = '';
+    data.moto_km = '';
+    data.moto_hora = '';
+    
+    $scope.closeAllModal();
+  };
+  
+ $scope.callGuincho = function(data){
+    //O usuário confirmando, 
+    //será apresentado uma tela onde ele 
+    //poderá confirmar a chegada do guincho 
+    //e realizar sua avaliação do serviço prestado.
+    
+    //TODO: Async call guincho   
+    $scope.openModal(4, data);
+    
+    //$scope.closeAllModal();
   };
 });
